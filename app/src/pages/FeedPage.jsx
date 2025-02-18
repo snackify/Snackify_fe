963a7c287f5fad05e961a56e87cad70918a35008
vet <vetin321@gmail.com>
2025-02-19 01:20:36 +0200

    fixed import

--- app/src/pages/FeedPage.jsx
+++ app/src/pages/FeedPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { fetchReviews, fetchFollowedUsers } from "../api";
 import ReviewList from "../components/ReviewList";
