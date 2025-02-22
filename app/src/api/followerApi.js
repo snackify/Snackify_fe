682b9722ff57d6dc40a24c8ad38fe2ff82de205e
vet <vetin321@gmail.com>
2025-02-22 13:49:15 +0200

    add before dockerization

--- app/src/api/followerApi.js
+++ app/src/api/followerApi.js
@@ -31,7 +31,25 @@ export const followUser = async (followeeName, user) => {
     return response.json();
   };
   
-  export const unfollowUser = async (followData) => {
+  export const unfollowUser = async (followeeName, user) => {
+
+    // get the followee ID from the followees username
+    const response_followee = await fetch(`http://localhost:8001/api/v1/users?username=${followeeName}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+      if (!response_followee.ok) {
+        throw new Error(`Failed to fetch user: ${response_followee.statusText}`);
+    }
+    const followee = await response_followee.json();
+    const followeeId = followee.user_id;
+
+    const followData = {
+      "follower_id": user.user_id,
+      "followee_id": followeeId
+    };
+
     const response = await fetch("http://localhost:8003/api/v1/follower", {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
